bd4eeba300963d38a665fd82d18c4ad986bd3961
Jarett <jarett.rude@gmail.com>
2020-04-23 00:43:11 -0700

    Update button.py
    - Converted GPIO setup to gpiozero library for ease of use.
    - Added a shutdown button
    - Added trigger button with LED
    - Added trigger singal to Arduino to control flash and countdown ring

--- button.py
+++ button.py
@@ -1,20 +1,43 @@
-#!/usr/bin/env python
-# Sends alt+p via GPIO24 to take a new picture
-import RPi.GPIO as GPIO
-import time
-import os
+#!/usr/bin/env python3
+#
+# Licensed under MIT license. See full license in LICENSE file.
+#
+# Author: Jarett Rude
+
+from gpiozero import LED, Button, DigitalOutputDevice
+from time import sleep
+from signal import pause
+from subprocess import check_call
 import uinput
 
-#print('Button Pressed')
+# SHUTDOWN BUTTON
+def shutdown():
+    check_call(['sudo', 'poweroff'])
+shutdown_btn = Button(21)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# DEFINE TIGGER BUTTON WITH LED
+triggerLED = LED(18)
+triggerButton = Button(4)
+
+# DEFINE SIGNAL TO ARDUINO FOR LIGHTS
+arduinoTrigger = DigitalOutputDevice(13,active_high=True,initial_value=False)
+def send():
+    arduinoTrigger.on()
+    #print(arduinoTrigger.value)
+    sleep(2)
+    arduinoTrigger.off()
 
 while True:
-    input_state = GPIO.input(24)
-    if input_state == False:
-        #print('Button Pressed') #<- for debugging only
+    triggerLED.on()
+    if triggerButton.is_pressed:
+        #print("pressed")
         with uinput.Device([uinput.KEY_ENTER]) as device:
-         time.sleep(1)
-         device.emit_combo([uinput.KEY_ENTER])
-         time.sleep(10)
+            sleep(1)
+            device.emit_combo([uinput.KEY_ENTER])
+            send()
+            sleep(8)
+    if shutdown_btn.is_pressed:
+        #print("shutdown")
+        shutdown()
+
+pause()
